895a73f9333b73a2119ac43a6bd64320f92954ba
Joan Perals <joan@perals.io>
2020-01-24 23:30:52 +0100

    Stub undo-redo store

---
+++ src/store/undo-redo.js
@@ -0,0 +1,30 @@
+export default {
+  state: {
+    snapshots: [],
+    snapshotIndex: null
+  },
+  getters: {
+    canUndo: state => {
+
+    },
+    canRedo: state => {
+
+    }
+  },
+  mutations: {
+    undo() {
+
+    },
+    redo() {
+
+    }
+  },
+  actions: {
+    undo() {
+
+    },
+    redo() {
+      
+    }
+  }
+}
