8b4ed3133770648ee574420c93ef965d3e52a07e
Joan Perals <joan@perals.io>
2020-01-26 13:45:18 +0100

    Fix syntax

--- src/components/toolbar-button-shapes.js
+++ src/components/toolbar-button-shapes.js
@@ -1,30 +1,32 @@
 export default function toolbarButtonShapes(color) {
   return [
-  {
-    type: "linear",
-    direction: "to bottom right",
-    stops: [
-      {
-        color: "transparent"
-      },
-      {
-        color: "transparent",
-        position: "15%"
-      },
-      {
-        color,
-        position: "15%"
-      },
-      {
-        color,
-        position: "65%"
-      },
-      {
-        color: "transparent",
-        position: "65%"
-      },
-      {
-        color: "transparent"
-      }
-    ]
-  }
+    {
+      type: "linear",
+      direction: "to bottom right",
+      stops: [
+        {
+          color: "transparent"
+        },
+        {
+          color: "transparent",
+          position: "15%"
+        },
+        {
+          color,
+          position: "15%"
+        },
+        {
+          color,
+          position: "65%"
+        },
+        {
+          color: "transparent",
+          position: "65%"
+        },
+        {
+          color: "transparent"
+        }
+      ]
+    }
+  ];
+}
