8cc870b5baf6591e45e7efa8f6f91418a40aeb0d
Joan Perals <joan@perals.io>
2020-02-24 22:58:33 +0100

    Find closest snap via binary search

--- src/store/snap.js
+++ src/store/snap.js
@@ -1,14 +1,55 @@
 export default {
   state: {
-    snapPoints: [],
+    snapPoints: { x: [], y: [] },
     snapThreshold: 5
   },
   getters: {
     snapPoint: state => point =>
-    findClosestSnap({ snapPointsSorted: state.snapPoints, point })
+      findClosestSnap({ snapPointsSorted: state.snapPoints, point })
   }
 };
 
-export function findClosestSnap({ snapPointsSorted, snapPoint }) {
+export function findClosestSnap({ snapPointsSorted, point }) {
+  return {
+    x: findClosestSnapInAxis({
+      snapPointsSorted: snapPointsSorted.x,
+      point: point.x
+    }),
+    y: findClosestSnapInAxis({
+      snapPointsSorted: snapPointsSorted.y,
+      point: point.y
+    })
+  };
+}
 
+// Performs binary search
+function findClosestSnapInAxis({
+  snapPointsSorted,
+  point,
+  start = 0,
+  end = snapPointsSorted.length - 1
+}) {
+  if (start == end) {
+    return snapPointsSorted[start];
+  } else if (end - start === 1) {
+    const first = snapPointsSorted[start];
+    const second = snapPointsSorted[end];
+    const distanceFirst = snapDistance({ snapPoint: first, number: point });
+    const distanceSecond = snapDistance({ snapPoint: second, number: point });
+    return distanceFirst <= distanceSecond ? first : second;
+  }
+  let indexAtHalf = start + Math.floor((end - start) / 2);
+  const valueAtHalf = snapPointsSorted[indexAtHalf].value;
+  const [newStart, newEnd] =
+    point <= valueAtHalf ? [start, indexAtHalf] : [indexAtHalf + 1, end];
+  return findClosestSnapInAxis({
+    snapPointsSorted,
+    point,
+    start: newStart,
+    end: newEnd
+  });
+}
+
+function snapDistance({ snapPoint, number }) {
+  return Math.abs(snapPoint.value - number);
 }
--- tests/unit/snap.spec.js
+++ tests/unit/snap.spec.js
@@ -3,29 +3,80 @@ import { findClosestSnap } from "@/store/snap";
 describe("findSnapPoint", () => {
   it("finds closest snap", () => {
     const snapPoints = {
-      x: [{ x: 3 }, { x: 10 }, { x: 16 }, { x: 17 }, { x: 34 }, { x: 34 }],
-      y: [{ y: 3 }, { y: 10 }, { y: 16 }, { y: 17 }, { y: 34 }, { y: 34 }]
+      x: [
+        { value: 3 },
+        { value: 10 },
+        { value: 16 },
+        { value: 17 },
+        { value: 34 },
+        { value: 34 }
+      ],
+      y: [
+        { value: 3 },
+        { value: 10 },
+        { value: 16 },
+        { value: 17 },
+        { value: 34 },
+        { value: 34 }
+      ]
     };
-    const point = {x: 10, y: 24};
-    const closestSnap = findClosestSnap({snapPointsSorted: snapPoints, point});
-    expect(closestSnap).toBe({x: 10, y: 17});
+    const point = { x: 10, y: 24 };
+    const closestSnap = findClosestSnap({
+      snapPointsSorted: snapPoints,
+      point
+    });
+    expect(closestSnap).toEqual({ x: { value: 10 }, y: { value: 17 } });
   });
   it("finds closest snap", () => {
     const snapPoints = {
-      x: [{ x: 3 }, { x: 10 }, { x: 16 }, { x: 17 }, { x: 34 }, { x: 34 }],
-      y: [{ y: 3 }, { y: 10 }, { y: 16 }, { y: 17 }, { y: 34 }, { y: 34 }]
+      x: [
+        { value: 3 },
+        { value: 10 },
+        { value: 16 },
+        { value: 17 },
+        { value: 34 },
+        { value: 34 }
+      ],
+      y: [
+        { value: 3 },
+        { value: 10 },
+        { value: 16 },
+        { value: 17 },
+        { value: 34 },
+        { value: 34 }
+      ]
     };
-    const point = {x: 1, y: 24};
-    const closestSnap = findClosestSnap({snapPointsSorted: snapPoints, point});
-    expect(closestSnap).toBe({x: 3, y: 17});
+    const point = { x: 1, y: 24 };
+    const closestSnap = findClosestSnap({
+      snapPointsSorted: snapPoints,
+      point
+    });
+    expect(closestSnap).toEqual({ x: { value: 3 }, y: { value: 17 } });
   });
   it("finds closest snap", () => {
     const snapPoints = {
-      x: [{ x: 3 }, { x: 10 }, { x: 16 }, { x: 17 }, { x: 34 }, { x: 34 }],
-      y: [{ y: 3 }, { y: 10 }, { y: 16 }, { y: 17 }, { y: 34 }, { y: 34 }]
+      x: [
+        { value: 3 },
+        { value: 10 },
+        { value: 16 },
+        { value: 17 },
+        { value: 34 },
+        { value: 34 }
+      ],
+      y: [
+        { value: 3 },
+        { value: 10 },
+        { value: 16 },
+        { value: 17 },
+        { value: 34 },
+        { value: 34 }
+      ]
     };
-    const point = {x: 10, y: 40};
-    const closestSnap = findClosestSnap({snapPointsSorted: snapPoints, point});
-    expect(closestSnap).toBe({x: 10, y: 34});
+    const point = { x: 10, y: 40 };
+    const closestSnap = findClosestSnap({
+      snapPointsSorted: snapPoints,
+      point
+    });
+    expect(closestSnap).toEqual({ x: { value: 10 }, y: { value: 34 } });
   });
 });
