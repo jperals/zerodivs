08302de1e8311da1d1867073332d4467bb288d8a
Joan Perals <joan@perals.io>
2020-03-09 23:23:17 +0100

    Improve CSS output a bit

--- src/components/ProjectOutput.vue
+++ src/components/ProjectOutput.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="output">
     <button class="close-button" v-on:click="close">Close</button>
-    <pre>{{ computedStyle }}</pre>
+    <pre>
+div {
+  {{ mainCustomStyle }}{{ mainStyle }}}
+div:before {
+{{ beforeStyle }}}
+div:after {
+{{ afterStyle }}}
+      </pre>
   </div>
 </template>
 
@@ -10,12 +17,56 @@ import shapes2css from "@/common/shapes2css";
 import store from "@/store";
 export default {
   computed: {
-    computedStyle() {
-      const shapes = store.getters.visibleShapes;
-      return shapes2css(shapes);
+    mainCustomStyle() {
+      return this.layerExtraStyles("main");
+    },
+    mainStyle() {
+      return shapes2css(this.shapesMain, "  ");
+    },
+    beforeStyle() {
+      const shapes = this.shapesBefore;
+      return this.layerExtraStyles("before") + shapes2css(shapes);
+    },
+    afterStyle() {
+      const shapes = this.shapesAfter;
+      return this.layerExtraStyles("after") + shapes2css(shapes);
+    },
+    isMainActive() {
+      return this.isLayerActive("main");
+    },
+    isBeforeActive() {
+      return this.isLayerActive("before");
+    },
+    isAfterActive() {
+      return this.isLayerActive("after");
+    },
+    shapesLayers() {
+      return store.getters.allLayers;
+    },
+    shapesMain() {
+      return this.getLayerShapes("main");
+    },
+    shapesBefore() {
+      return this.getLayerShapes("before");
+    },
+    shapesAfter() {
+      return this.getLayerShapes("after");
     }
   },
   methods: {
+    getLayerShapes(layerName) {
+      return this.shapesLayers[layerName].shapes;
+    },
+    isLayerActive(layerName) {
+      return this.shapesLayers[layerName].active;
+    },
+    layerExtraStyles(layerName) {
+      const rawString = this.shapesLayers[layerName].extraStyles || "";
+      return rawString.replace(
+        /\n[^$]/g,
+        (match, char) => "\n  " + rawString[char + 1]
+      );
+    },
     close() {
       store.dispatch("toggleOutput", false);
     }
@@ -42,9 +93,9 @@ export default {
 pre {
   background-color: hsl(200, 50%, 95%);
   padding: 1rem;
-  border-radius: .25rem;
+  border-radius: 0.25rem;
   border: 1px solid var(--panel-border-color);
   margin-bottom: 0;
-  white-space: pre-line;
+  white-space: pre-wrap;
 }
 </style>
