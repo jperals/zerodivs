71cc843428d9e5e7dd100c9962593574889be2c2
Joan Perals <joan@perals.io>
2020-06-13 00:40:18 +0200

    Fix CloseButton's rotated visible focus area

--- src/components/CloseButton.vue
+++ src/components/CloseButton.vue
@@ -1,21 +1,48 @@
 <template>
-  <AddButton :onClick="onClick" />
+  <button v-on:click="onClick" />
 </template>
 
 <script>
-import AddButton from "@/components/AddButton";
 export default {
-  components: {
-    AddButton
-  },
   props: {
     onClick: Function
   }
-}
+};
 </script>
 
 <style scoped>
-  button {
-    transform: rotate(45deg);
-  }
+button {
+  background-color: var(--gray-400);
+  position: relative;
+  border: 0 none;
+}
+button:hover {
+  background-color: var(--gray-500);
+}
+button:before {
+  background-image: linear-gradient(
+      transparent 40%,
+      white 40%,
+      white 60%,
+      transparent 60%
+    ),
+    linear-gradient(
+      to right,
+      transparent 40%,
+      white 40%,
+      white 60%,
+      transparent 60%
+    );
+  background-position: center center, center center;
+  background-size: 60% 60%, 60% 60%;
+  background-repeat: no-repeat;
+  transform: rotate(45deg);
+  display: block;
+  content: '';
+  width: 100%;
+  height: 100%;
+  position: relative;
+  left: 0;
+  top: 0;
+}
 </style>
