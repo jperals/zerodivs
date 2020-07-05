fccc55f8b773f1bc64c9ec0dbec5df87c16d427d
Joan Perals <joan@perals.io>
2020-07-05 11:33:11 +0200

    Add script to publish to gh-pages

---
+++ publish_to_gh_pages.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf dist
+mkdir dist
+git worktree prune
+rm -rf .git/worktrees/dist/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages dist github/gh-pages
+
+echo "Removing existing files"
+rm -rf dist/*
+
+echo "Generating site"
+npm run build
+
+echo "Updating gh-pages branch"
+cd dist && git add --all && git commit -m "New build"
+
+echo "Publishing to Github pages"
+git push && cd ..
